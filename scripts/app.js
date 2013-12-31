b16a644019d0e746d6b6c09532be764dc29c16ca
Clemens Brunner <clemens@hans.(none)>
2013-12-31 15:48:25 +0100

    auto

--- scripts/app.js
+++ scripts/app.js
@@ -1,27 +1,32 @@
 var app = angular.module('truckApp', []);
 
 function InputDataCtrl($scope, $http) {
-     $scope.loadData = function() {
-     $http.jsonp('http://mtoserver.dyndns.org/mto/sql.php?callback=JSON_CALLBACK'
-     ).then(function(response) {
-     $scope.inputDatas = response.data;
-     getData();
-     });
-     };
+	$scope.loadData = function() {
+		$http
+				.jsonp(
+						'http://mtoserver.dyndns.org/mto/sql.php?callback=JSON_CALLBACK')
+				.then(function(response) {
+					$scope.inputDatas = response.data;
+					getData();
+				});
+	};
 
-    $scope.loadData();
+	$scope.loadData();
 }
 
-function sendData(formId) { 
-	//validation Code
-    $.ajax( {
-            type: 'POST',
-            url: 'http://mtoserver.dyndns.org/mto/formToMail.php',
-            data: $("#"+formId).serialize(), 
-            success: function(data) {
-                alert("data send");
-            }
-        } );
-	header('Location: index.html');
-    }
-
+function sendData(formId) {
+	// validation Code
+	$.ajax({
+		type : 'POST',
+		url : 'http://mtoserver.dyndns.org/mto/formToMail.php',
+		data : $("#" + formId).serialize(),
+		success : function(data) {
+			work = 1;
+		},
+		error : function(responseData, textStatus, errorThrown) {
+			work = 0;
+		}
+	});
+	window.history.go(-1);
+		alert("Die Mitarbeiter von MTO melden sich in Kürze");
+}
