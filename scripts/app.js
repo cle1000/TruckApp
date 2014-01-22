d8bb2b63005373b142de2a98e7c50e1158abfc04
Clemens Brunner <clemens@hans.(none)>
2014-01-22 16:37:27 +0100

    auto

--- scripts/app.js
+++ scripts/app.js
@@ -7,42 +7,66 @@ function InputDataCtrl($scope, $http) {
 						'http://mtoserver.dyndns.org/mto/sql.php?callback=JSON_CALLBACK')
 				.then(function(response) {
 					$scope.inputDatas = response.data;
-					getData();
 				});
 	};
+	
+	$scope.setFirstStart = function () {
+		window.localStorage.setItem("init", "1");
+	};
+	
+	$scope.setFirstStartBtn = function () {
+		$scope.setFirstStart();
+		document.location.hash = 'home';
+	};
+	
+	$scope.saveUserInfos = function() {
+		window.localStorage.setItem("name", $scope.username);
+		window.localStorage.setItem("telephone", $scope.userphone);
+	};
+	
+	$scope.saveUserInfosBtn = function() {
+		$scope.saveUserInfos();
+		$scope.setFirstStart();
+		document.location.hash = 'home';
+	};
+
+	$scope.sendData = function(formId) {
+		var form = document.getElementById(formId);
+
+		$scope.saveUserInfos();
+
+		/*
+		 * $.ajax({ type : 'POST', url :
+		 * 'http://mtoserver.dyndns.org/mto/formToMail.php', data : $("#" +
+		 * formId).serialize(), success : function(data) { alert ("success"); }
+		 * });
+		 */
+
+		window.history.go(-1);
+		showAlert("Die Mitarbeiter von MTO melden sich in Kürze", "Info");
+	};
+
+	$scope.setNameAndTelephone = function() {
+		$scope.username = window.localStorage.getItem("name");
+		$scope.userphone = window.localStorage.getItem("telephone");
+	};
+
+
+	var init = window.localStorage.getItem("init");
+	
+	if (init == 1){
+		$(".first_start").remove();
+		$scope.setNameAndTelephone();
+		alert ("bla");
+	}
 
 	$scope.loadData();
 }
 
-function showAlert (message, title){
+function showAlert(message, title) {
 	if (navigator.notification) {
 		navigator.notification.alert(message, null, title, 'OK');
 	} else {
 		alert(title ? (title + ": " + message) : message);
 	}
 }
-
-function sendData(formId) {
-	// validation Code
-	var form = document.getElementById(formId);
-	
-	var name = form.elements["name"].value;
-	var telephone = form.elements["telephone"].value;
-	window.localStorage.setItem("name", name);
-	window.localStorage.setItem("telephone", telephone);
-	
-	/*$.ajax({
-		type : 'POST',
-		url : 'http://mtoserver.dyndns.org/mto/formToMail.php',
-		data : $("#" + formId).serialize(),
-		success : function(data) {
-				alert ("success");
-		}
-	});*/
-
-	window.history.go(-1);
-	showAlert("Die Mitarbeiter von MTO melden sich in Kürze", "Info");
-}
-
-
-
--- scripts/initfirstpage.js
+++ scripts/initfirstpage.js
@@ -4,39 +4,7 @@ function setFirstStart(){
 	window.localStorage.setItem("init", "1");
 }
 
-function saveUserInfos(){
-	var name = document.getElementById("name_input").value;
-	var telephone = document.getElementById("telephone_input").value;
-	
-	window.localStorage.setItem("name", name);
-	window.localStorage.setItem("telephone", telephone);
-	setNameAndTelephone();
-	setFirstStart();
-}
 
-
-
-function setNameAndTelephone (){
-	var name = window.localStorage.getItem("name");
-	var telephone = window.localStorage.getItem("telephone");
-
-	if (name == null){
-		name = '';
-	}
-	if (telephone == null){
-		telephone = '';
-	}
-
-	var nameElements = document.getElementsByClassName('name');
-	var telephoneElements = document.getElementsByClassName('telephone');
-
-	for (var i = 0; (element = nameElements[i]) != null; i++){
-		element.setAttribute('value', name);
-	}
-	for (var i = 0; (element = telephoneElements[i]) != null; i++){
-		element.setAttribute('value', telephone);
-	}
-}
 
 
 if (init != 1){
@@ -45,7 +13,7 @@ if (init != 1){
 	startPage.setAttribute ('data-role', 'page');
 	startPage.setAttribute ('id', 'first_start');
 	
-	startPage.innerHTML = '<div data-role="header" data-position="fixed">	<h1>mto</h1></div><!-- /header --><div role="main" class="ui-content"><div class="ui-collapsible-set ui-collapsible ui-collapsible-themed-content ui-collapsible-inset ui-corner-all"><div data-insert="true" class="ui-collapsible-content ui-body-inherit">	<form action="#home" method="post" id="first_infos">			<fieldset>				<legend class="first">Ihre Daten...</legend>				<label for="name_input">Name</label> <input name="name"					id="name_input" type="text" placeholder="Name" /> <label					for="telephone_input">Telefon</label> <input name="telephone"					id="telephone_input" type="text" placeholder="Telefon">			</fieldset>				<div class="ui-grid-a">			<div class="ui-block-a">				<button class="ui-shadow ui-btn ui-corner-all" type="submit"					onClick="saveUserInfos()" id="submit_input">Speichern</button>			</div>			<div class="ui-block-b">				<button class="ui-shadow ui-btn ui-corner-all" type="submit"					onClick="setFirstStart()" id="submit_input">Überspringen</button>			</div>		</div>	</form></div></div></div>';
+	startPage.innerHTML = '<div data-role="header" data-position="fixed">	<h1>mto</h1></div><!-- /header --><div role="main" class="ui-content"><div class="ui-collapsible-set ui-collapsible ui-collapsible-themed-content ui-collapsible-inset ui-corner-all"><div data-insert="true" class="ui-collapsible-content ui-body-inherit">	<form action="#home" method="post" id="first_infos">			<fieldset>				<legend class="first">Ihre Daten...</legend>				<label for="name_input">Name</label> <input name="name"					id="name_input" type="text" placeholder="Name" /> <label					for="telephone_input">Telefon</label> <input name="telephone"					id="telephone_input" type="number" placeholder="Telefon">			</fieldset>				<div class="ui-grid-a">			<div class="ui-block-a">				<button class="ui-shadow ui-btn ui-corner-all" type="submit"					onClick="saveUserInfos()" id="submit_input">Speichern</button>			</div>			<div class="ui-block-b">				<button class="ui-shadow ui-btn ui-corner-all" type="submit"					onClick="setFirstStart()" id="submit_input">Überspringen</button>			</div>		</div>	</form></div></div></div>';
 	
 	element.insertBefore(startPage, element.firstChild);
 }else{
