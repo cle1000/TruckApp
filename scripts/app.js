3430d42ae632e01b125a2ce73087fd4195ef705c
Clemens Brunner <clemens@hans.(none)>
2014-01-25 21:35:32 +0100

    auto

--- scripts/app.js
+++ scripts/app.js
@@ -2,7 +2,7 @@ var app = angular.module('truckApp', []);
 
 function InputDataCtrl($scope, $http) {
 	$scope.goHome = function (){
-		$.mobile.changePage("index1.html#home");
+		$.mobile.changePage("index1.html#home", null, true, true);
 		$scope.$apply();
 	};
 
