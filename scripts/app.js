8a2e4fa38c86c04147ee970b66e5ff309e7fe678
Clemens Brunner <clemens@hans.(none)>
2014-02-26 20:27:14 +0100

    auto

--- scripts/app.js
+++ scripts/app.js
@@ -1,83 +1,82 @@
 var app = angular.module('truckApp', []);
 
-function InputDataCtrl($scope, $http) {
-	$scope.goHome = function (){
-		$.mobile.changePage("#home");
-	};
-
-	$scope.loadData = function() {
-		$http.jsonp('http://mtoserver.dyndns.org/mto/sql.php?callback=JSON_CALLBACK').then(function(response) {
-			$scope.inputDatas = response.data;
-		});
-	};
-
-	$scope.setFirstStart = function() {
-		window.localStorage.setItem("init", "1");
-	};
-
-	$scope.setFirstStartBtn = function() {
-		$scope.setFirstStart();
-		$scope.goHome();
-	};
-
-	$scope.saveUserInfos = function() {
-		window.localStorage.setItem("name", $scope.username);
-		window.localStorage.setItem("telephone", $scope.userphone);
-	};
-
-	$scope.saveUserInfosBtn = function() {
-		$scope.saveUserInfos();
-		$scope.setFirstStart();
-		$scope.goHome();
-	};
-
-	$scope.sendData = function(formId) {
-		var form = document.getElementById(formId);
-
-		$scope.saveUserInfos();
-
-		$.ajax({
-			type : 'POST',
-			url : 'http://mtoserver.dyndns.org/mto/formToMail.php',
-			data : $("#" + formId).serialize(),
-			success : function(data) {
-			}
-		});
-
-		$scope.goHome();
-		showAlert("Die Mitarbeiter von MTO melden sich in Kürze", "Info");
-	};
-
-	$scope.setNameAndTelephone = function() {
-		var name = window.localStorage.getItem("name");
-		var phone = window.localStorage.getItem("telephone");
-
-		if (name == 'null' || name == 'undefined') {
-			name = '';
-		}
-		if (phone == 'null' || phone == 'undefined') {
-			phone = '';
-		}
-		$scope.username = name;
-		$scope.userphone = phone;
-	};
-
-	var init = window.localStorage.getItem("init");
-	init = 1;
-	$scope.loadData();
-
-	if (init == 1) {
-		$scope.setNameAndTelephone();
-		$('.first_start').attr("data-role","nopage");
-	}
-
-
+function showAlert(message, title) {
+    if (navigator.notification) {
+        navigator.notification.alert(message, null, title, 'OK');
+    } else {
+        alert(title ? (title + ": " + message) : message);
+    }
 }
 
-function showAlert(message, title) {
-	if (navigator.notification) {
-		navigator.notification.alert(message, null, title, 'OK');
-	} else {
-		alert( title ? (title + ": " + message) : message);
-	}
+function InputDataCtrl($scope, $http) {
+    $scope.goHome = function() {
+        $.mobile.changePage("#home");
+    };
+
+    $scope.loadData = function() {
+        $http.jsonp('http://mtoserver.dyndns.org/mto/sql.php?callback=JSON_CALLBACK').then(function(response) {
+            $scope.inputDatas = response.data;
+        });
+    };
+
+    $scope.setFirstStart = function() {
+        window.localStorage.setItem("init", "1");
+    };
+
+    $scope.setFirstStartBtn = function() {
+        $scope.setFirstStart();
+        $scope.goHome();
+    };
+
+    $scope.saveUserInfos = function() {
+        window.localStorage.setItem("name", $scope.username);
+        window.localStorage.setItem("telephone", $scope.userphone);
+    };
+
+    $scope.saveUserInfosBtn = function() {
+        $scope.saveUserInfos();
+        $scope.setFirstStart();
+        $scope.goHome();
+    };
+
+    $scope.sendData = function(formId) {
+        var form = document.getElementById(formId);
+
+        $scope.saveUserInfos();
+
+        $.ajax({
+            type: 'POST',
+            url: 'http://mtoserver.dyndns.org/mto/formToMail.php',
+            data: $("#" + formId).serialize(),
+            success: function(data) {
+
+            }
+        });
+
+        $scope.goHome();
+        showAlert("Die Mitarbeiter von MTO melden sich in Kürze", "Info");
+    };
+
+    $scope.setNameAndTelephone = function() {
+        var name = window.localStorage.getItem("name");
+        var phone = window.localStorage.getItem("telephone");
+
+        if (name == 'null' || name == 'undefined') {
+            name = '';
+        }
+        if (phone == 'null' || phone == 'undefined') {
+            phone = '';
+        }
+        $scope.username = name;
+        $scope.userphone = phone;
+    };
+
+    var init = window.localStorage.getItem("init");
+    init = 1;
+    $scope.loadData();
+
+    if (init == 1) {
+        $scope.setNameAndTelephone();
+        $('.first_start').attr("data-role", "nopage");
+    }
 }
